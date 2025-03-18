a1a13ef2ee8cf58531da434f97d6b527b7358001
DzMaks <rattlemaniac@gmail.com>
2025-03-18 20:53:05 +0300

    Добавил сортировки

--- src/main/java/com/aston/homework3/App.java
+++ src/main/java/com/aston/homework3/App.java
@@ -4,19 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
-      MyLinkedList<Integer> list = new MyLinkedList<>();
-      list.add(43);
-      list.add(98);
-      list.add(25);
-      list.add(45);
-      list.add(102);
-      list.add(15);
-      list.add(7);
-      list.add(65);
-      list.add(17);
-     System.out.println(list);
-     list.quickSort();
-     System.out.println(list);
+      
 
     }
 
